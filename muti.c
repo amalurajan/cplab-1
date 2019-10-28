f8f066dba61f8e310571786d5f1fc743556a436c
amalurajan <47589248+amalurajan@users.noreply.github.com>
2019-10-28 11:19:30 +0530

    Update muti.c

--- muti.c
+++ muti.c
@@ -4,12 +4,12 @@ DATE:5/03/19*/
 #include<stdio.h>
 void main()
 {
-int num,i=1,product;
+int num,i=0,product;
 printf("enter the number");
 scanf("%d",&num);
-for(i=1;i<=10;i++)
+for(i=0;i<10;i++)
 	{
-	product=num*i;
+	product=num*(i+1);
 	printf("%d*%d=%d\n",num,i,product);
 	}
 }
